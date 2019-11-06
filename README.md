6d3a4a09ef268850bb96a6149228b8517f50e500
imcaspar <casparz@gmail.com>
2019-11-06 13:15:29 +0800

    init commit

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
-# gpt2-ml
+![gpt2-ml](./.github/logo.png)
+# GPT2 for Multiple Languages
+
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/imcaspar/gpt2-ml/blob/master/pretrained_model_demo.ipynb)
 ![GitHub](https://img.shields.io/github/license/imcaspar/gpt2-ml)
 ![GitHub All Releases](https://img.shields.io/github/downloads/imcaspar/gpt2-ml/total)
 [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/imcaspar/gpt2-ml/issues)
 ![GitHub stars](https://img.shields.io/github/stars/imcaspar/gpt2-ml?style=social)
 ![Twitter Follow](https://img.shields.io/twitter/follow/imcaspar?style=social)
 
-GPT2 for Multiple Languages
+[**中文说明**](./README.md) | [**English**](./README_EN.md)
+
+- [x] 简化整理 GPT2 训练代码（based on grover）
+- [x] 移植 bert tokenizer，添加多语言支持
+- [x] 15亿参数 GPT2 中文预训练模型
+
+
+## 预训练模型
+15亿参数中文预训练模型 [Google Drive 下载](https://drive.google.com/open?id=1_6Py_UEGSAMt2RCq_dxsGNfpF4jMhm-5)
+
+训练语料来自 [THUCNews](http://thuctc.thunlp.org/#%E4%B8%AD%E6%96%87%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB%E6%95%B0%E6%8D%AE%E9%9B%86THUCNews) 以及 [nlp_chinese_corpus](https://github.com/brightmart/nlp_chinese_corpus)，清洗后总文本量约 15G 
+
+使用 [Cloud TPU Pod v3-256](https://cloud.google.com/tpu/docs/types-zones#types) 训练 10w 步
+
+![loss](./.github/loss.png)
+
+
+## Google Colab
+
+
+## 训练
+
+
+## Reference
+https://github.com/google-research/bert
+
+https://github.com/rowanz/grover
 
 Research supported with Cloud TPUs from Google's TensorFlow Research Cloud (TFRC)
---
+++ scripts/contextual_generate_cli.py
@@ -0,0 +1,177 @@
+
+import tensorflow as tf
+import numpy as np
+import sys
+import json
+import sys
+
+sys.path.append('./')
+from train.modeling import GroverModel, GroverConfig, _top_p_sample, sample
+from tqdm import tqdm
+
+import argparse
+
+from tokenization import tokenization
+
+parser = argparse.ArgumentParser(description='Contextual generation (aka given some metadata we will generate articles')
+parser.add_argument(
+    '-metadata_fn',
+    dest='metadata_fn',
+    type=str,
+    help='Path to a JSONL containing metadata',
+)
+parser.add_argument(
+    '-out_fn',
+    dest='out_fn',
+    type=str,
+    help='Out jsonl, which will contain the completed jsons',
+)
+parser.add_argument(
+    '-input',
+    dest='input',
+    type=str,
+    help='Text to complete',
+)
+parser.add_argument(
+    '-model_config_fn',
+    dest='model_config_fn',
+    default='configs/mega.json',
+    type=str,
+    help='Configuration JSON for the model',
+)
+parser.add_argument(
+    '-model_ckpt',
+    dest='model_ckpt',
+    default='../models/base/model.ckpt',
+    type=str,
+    help='checkpoint file for the model',
+)
+parser.add_argument(
+    '-target',
+    dest='target',
+    default='article',
+    type=str,
+    help='What to generate for each item in metadata_fn. can be article (body), title, etc.',
+)
+parser.add_argument(
+    '-batch_size',
+    dest='batch_size',
+    default=1,
+    type=int,
+    help='How many things to generate per context. will split into chunks if need be',
+)
+parser.add_argument(
+    '-num_folds',
+    dest='num_folds',
+    default=1,
+    type=int,
+    help='Number of folds. useful if we want to split up a big file into multiple jobs.',
+)
+parser.add_argument(
+    '-fold',
+    dest='fold',
+    default=0,
+    type=int,
+    help='which fold we are on. useful if we want to split up a big file into multiple jobs.'
+)
+parser.add_argument(
+    '-max_batch_size',
+    dest='max_batch_size',
+    default=None,
+    type=int,
+    help='max batch size. You can leave this out and we will infer one based on the number of hidden layers',
+)
+parser.add_argument(
+    '-top_p',
+    dest='top_p',
+    default=0.95,
+    type=float,
+    help='p to use for top p sampling. if this isn\'t none, use this for everthing'
+)
+parser.add_argument(
+    '-samples',
+    dest='samples',
+    default=1,
+    type=int,
+    help='num_samples',
+)
+
+def extract_generated_target(output_tokens, tokenizer):
+    """
+    Given some tokens that were generated, extract the target
+    :param output_tokens: [num_tokens] thing that was generated
+    :param encoder: how they were encoded
+    :param target: the piece of metadata we wanted to generate!
+    :return:
+    """
+    # Filter out first instance of start token
+    assert output_tokens.ndim == 1
+
+    start_ind = 0
+    end_ind = output_tokens.shape[0]
+
+    return {
+        'extraction': tokenization.printable_text(''.join(tokenizer.convert_ids_to_tokens(output_tokens))),
+        'start_ind': start_ind,
+        'end_ind': end_ind,
+    }
+
+args = parser.parse_args()
+
+tokenizer = tokenization.FullTokenizer(vocab_file="bert-base-chinese-vocab.txt" , do_lower_case=True)
+news_config = GroverConfig.from_json_file(args.model_config_fn)
+
+# We might have to split the batch into multiple chunks if the batch size is too large
+default_mbs = {12: 32, 24: 16, 48: 3}
+max_batch_size = args.max_batch_size if args.max_batch_size is not None else default_mbs[news_config.num_hidden_layers]
+
+# factorize args.batch_size = (num_chunks * batch_size_per_chunk) s.t. batch_size_per_chunk < max_batch_size
+num_chunks = int(np.ceil(args.batch_size / max_batch_size))
+batch_size_per_chunk = int(np.ceil(args.batch_size / num_chunks))
+print("\n~~\nbatch size={}, max batch size={}, num chunks={}, batch size per chunk={}\n~~\n".format(
+    args.batch_size, max_batch_size, num_chunks, batch_size_per_chunk), flush=True)
+
+# This controls the top p for each generation.
+top_p = np.ones((num_chunks, batch_size_per_chunk), dtype=np.float32) * args.top_p
+
+tf_config = tf.ConfigProto(allow_soft_placement=True)
+
+with tf.Session(config=tf_config, graph=tf.Graph()) as sess:
+    initial_context = tf.placeholder(tf.int32, [batch_size_per_chunk, None])
+    p_for_topp = tf.placeholder(tf.float32, [batch_size_per_chunk])
+    eos_token = tf.placeholder(tf.int32, [])
+    tokens, probs = sample(news_config=news_config, initial_context=initial_context,
+                           eos_token=eos_token, ignore_ids=None, p_for_topp=p_for_topp,
+                           do_topk=False)
+
+    saver = tf.train.Saver()
+    saver.restore(sess, args.model_ckpt)
+    print('Loaded model.')
+    text = input()
+    while text != "":
+        for i in range(args.samples):
+            print("Sample,", i + 1, " of ", args.samples)
+            # Let's go!
+            line = tokenization.convert_to_unicode(text)
+            bert_tokens = tokenizer.tokenize(line)
+            encoded = tokenizer.convert_tokens_to_ids(bert_tokens)
+            context_formatted = []
+            context_formatted.extend(encoded)
+            # Format context end
+
+            gens = []
+            gens_raw = []
+            gen_probs = []
+
+            for chunk_i in range(num_chunks):
+                tokens_out, probs_out = sess.run([tokens, probs],
+                                                 feed_dict={initial_context: [context_formatted] * batch_size_per_chunk,
+                                                            eos_token: 60000,
+                                                            p_for_topp: top_p[chunk_i]})
+
+                for t_i, p_i in zip(tokens_out, probs_out):
+                    extraction = extract_generated_target(output_tokens=t_i, tokenizer=tokenizer)
+                    gens.append(extraction['extraction'])
+
+            print(gens[0])
+        text = input()
